7427e203bf5347413ee2a4225bb35e213156bbd3
Ilya Zheman <zheman@yohoho.games>
2024-12-08 09:43:50 +0300

    Test ML Agents

---
+++ Assets/Scripts/Test/MoveToGoalAgent.cs
@@ -0,0 +1,14 @@
+﻿using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using UnityEngine;
+
+namespace Test
+{
+    public class MoveToGoalAgent : Agent
+    {
+        public override void OnActionReceived(ActionBuffers actions)
+        {
+            Debug.Log(actions.DiscreteActions[0]);
+        }
+    }
+}
